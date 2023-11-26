597278c77fd1ccd3044ff72fbef7d5a1e24380ee
PaysanCorrezien <108607151+PaysanCorrezien@users.noreply.github.com>
2023-11-26 22:01:50 +0100

    added readme, and first draft install script

---
+++ Install.ps1
@@ -0,0 +1,170 @@
+# For no prompt install
+param (
+    [switch]$Force
+)
+
+# This one use /releases, /latest is more commonly used but dont work for Yazi
+function Get-GithubRelease {
+    param (
+        [string]$Repo
+    )
+
+    try {
+        $uri = "https://api.github.com/repos/$Repo/releases"
+        Write-Host "Requesting URL: $uri"
+        $response = Invoke-RestMethod -Uri $uri -Method Get -Headers @{ Accept = "application/vnd.github.v3+json" }
+        
+        if ($response.Count -gt 0) {
+            $latestRelease = $response | Sort-Object -Property published_at -Descending | Select-Object -First 1
+            Write-Host "Latest release found: Tag $($latestRelease.tag_name), Published on $($latestRelease.published_at)"
+            return $latestRelease.tag_name
+        } else {
+            Write-Host "No releases found for $Repo."
+            return $null
+        }
+    }
+    catch {
+        Write-Host "Error encountered: $_"
+        return $null
+    }
+}
+
+
+function Get-GithubRepo {
+    param (
+        [string]$Repo,
+        [string]$TagName,
+        [string]$DestinationPath,
+        [string]$Platform = "windows"  # Default to 'windows'. Change as needed.
+    )
+
+    try {
+        $uri = "https://api.github.com/repos/$Repo/releases/tags/$TagName"
+        $response = Invoke-RestMethod -Uri $uri -Method Get -Headers @{ Accept = "application/vnd.github.v3+json" }
+
+        $asset = $response.assets | Where-Object { $_.name -like "*$Platform*.zip" } | Select-Object -First 1
+
+        if ($asset) {
+            $tempPath = [System.IO.Path]::GetTempPath()
+            $tempFile = Join-Path $tempPath $asset.name
+            $downloadUrl = $asset.browser_download_url
+
+            Write-Host "Downloading ZIP to temporary folder: $tempFile"
+            Invoke-WebRequest -Uri $downloadUrl -OutFile $tempFile
+
+            Write-Host "Extracting ZIP to $DestinationPath"
+            Expand-Archive -LiteralPath $tempFile -DestinationPath $DestinationPath -Force
+
+            Write-Host "Cleaning up temporary file."
+            Remove-Item -Path $tempFile
+
+            return $DestinationPath
+        }
+        else {
+            Write-Error "No downloadable ZIP asset found for $Platform platform in $Repo release $TagName"
+            return $null
+        }
+    }
+    catch {
+        Write-Host "Error encountered: $_"
+        return $null
+    }
+}
+
+
+function Add-FolderPathToEnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$folderPath
+    )
+
+    try {
+        # Check if the folder path exists
+        if (-Not (Test-Path -Path $folderPath)) {
+            throw "Folder path does not exist: $folderPath"
+        }
+
+        # Get the current PATH environment variable for the user
+        $currentPath = [System.Environment]::GetEnvironmentVariable("PATH", "User")
+
+        # Check if the folder path is already in the PATH
+        if ($currentPath -split ';' -contains $folderPath) {
+            Write-Host "Folder path already in PATH: $folderPath"
+            return
+        }
+
+        # Add folder path to the PATH environment variable
+        $newPath = $currentPath + ';' + $folderPath
+        [System.Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
+
+        # Verify if the folder path was added
+        $updatedPath = [System.Environment]::GetEnvironmentVariable("PATH", "User")
+        if ($updatedPath -split ';' -contains $folderPath) {
+            Write-Host "Folder path added to PATH successfully: $folderPath"
+        } else {
+            throw "Failed to add folder path to PATH"
+        }
+    }
+    catch {
+        Write-Error "An error occurred: $_"
+    }
+}
+function Create-WeztermYaziShortcut {
+    $desktopPath = [System.Environment]::GetFolderPath('Desktop')
+    $shortcutPath = Join-Path $desktopPath "WezTerm Yazi.lnk"
+
+    $WshShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($shortcutPath)
+    
+    $Shortcut.TargetPath = "C:\Program Files\WezTerm\wezterm-gui.exe"
+    $Shortcut.Arguments = "-e pwsh.exe -NoExit -Command `"& { `$Host.UI.RawUI.WindowTitle = 'TUI File Explorer - Yazi';  yazi }`""
+    $Shortcut.WorkingDirectory = "C:\Program Files\WezTerm"
+    $Shortcut.IconLocation = "C:\Program Files\WezTerm\wezterm-gui.exe"
+    $Shortcut.Save()
+
+    Write-Host "Shortcut created at $shortcutPath"
+}
+
+function Get-UserConfirmation {
+    param (
+        [string]$Message
+    )
+    $userInput = Read-Host -Prompt $Message
+    return $userInput -eq 'Y'
+}
+
+
+
+# Start of the program
+# user/appdata/local/ folder is more common for program, maybe change this ?
+$Repo = "sxyazi/yazi"
+$destinationPath = "$env:APPDATA\yazi\bin"
+
+# Check and create destination folder
+if (-not (Test-Path -Path $destinationPath)) {
+    New-Item -ItemType Directory -Path $destinationPath | Out-Null
+    Write-Host "Destination folder created: $destinationPath"
+} else {
+    Write-Host "Destination folder already exists: $destinationPath"
+}
+
+# Installation Process (Get release, Download, Install)
+if ($Force -or (Get-UserConfirmation "Proceed with installation for $Repo? (Y/N)")) {
+    $latestRelease = Get-GithubRelease -Repo $Repo
+
+    if ($latestRelease) {
+        Get-GithubRepo -Repo $Repo -TagName $latestRelease -DestinationPath $destinationPath -Force:$Force
+    } else {
+        Write-Host "No latest release found for $Repo."
+    }
+}
+
+# Prompt for adding to environment path
+if ($latestRelease -and ($Force -or (Get-UserConfirmation "Do you want to add $destinationPath to your environment path? (Y/N)"))) {
+    Add-FolderPathToEnvPath -folderPath $destinationPath
+    Write-Host "$destinationPath has been added to your environment path."
+}
+# Prompt for creating WezTerm Yazi Shortcut
+if ($Force -or (Get-UserConfirmation "Do you want to create a WezTerm Yazi shortcut on your desktop? (Y/N)")) {
+    Create-WeztermYaziShortcut
+}
---
+++ README.md
@@ -0,0 +1,93 @@
+
+This repo include the scripts i use to make [yazi](https://github.com/sxyazi/yazi) even better on Windows
+
+# 🔖 Prerequisite
+
+- File ( from git for mimetype) ,
+- Wezterm for terminal image preview : [wezterm](https://wezfurlong.org/wezterm/) 
+- Pwsh aka Powershell Core or you will need to replace pwsh.exe by powershell.exe on every script
+All is mentionned on the [Installation Guide](https://github.com/sxyazi/yazi/wiki/Windows-Installation-Guide) 
+
+# 🪄 Yazi Installation Script
+
+The installation script will cover the installation of the program,
+adding it to `PATH` ( allowing system to use it) and creation of a shortcut to launch it with wezterm,
+_This script doesn't install the recommanded package like fzf zoxide_
+The icon is created With default wezterm path from `winget` installation, if you use a portable version adjust it
+
+## 👷 Using the script
+
+Open powershell and run
+
+```powershell
+iex (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/PaysanCorrezien/Yazi-Windows/main/install.ps1' -UseBasicParsing).Content
+```
+
+*Always check script before running it from a random repo like mine*
+
+_Maybe adding the copy of the preset from [repo](https://github.com/sxyazi/yazi/tree/main/yazi-config/preset) would be good_
+
+# 🌟 Features enhancement
+
+This repo include multiple script that offer some functionnality that are realy usefull on windows explorer :
+
+- Open Details tab of a file
+- Open powershell in current dir
+- Open powershell in current dir as admin ( UAC will prompt ! )
+- Drag and drop one or multiple files
+- Open explorer in current working directory
+ 
+And one more that i like a lot and is working perfectly for my use case :
+- Open In WSLTmux , which create a session in tmux with the name of file as session name, cd into the directory of the file, and open it in Neovim 
+This one is working on my `special` use case but can give you inspiration to make something that feet your need.
+
+## How to install
+
+Open powershell and clone the repo :
+```powershell
+$dirPath = "$env:APPDATA\yazi\WindowsEnhancement"; New-Item -ItemType Directory -Path $dirPath -Force | Out-Null; git clone "https://github.com/PaysanCorrezien/Yazi-Windows.git" $dirPath
+```
+## Using it
+
+To use those function we need to edit the keymap.toml file provided by the preset [here](https://github.com/sxyazi/yazi/tree/main/yazi-config/preset) 
+On my `keymap.toml` on `$env:APPDATA\yazi\config\` , on the keymap array at the end i added :
+```toml
+  # Open windows Explorer in current working directory ( for features missing in yazi for now)
+  	{ on = ["g", "e" ], exec = "shell 'explorer .'", desc = "Open Explorer here" },
+  # Allow to drag one or multiples Files like dragon on linux. To copy in emails for example 
+  # remove --confirm to check the command before running it
+    { on = [ "c", "D" ], exec= '''
+    shell 'pwsh.exe -ex Bypass -C "& $env:APPDATA\yazi\WindowsEnhancement\Drag.ps1 %*"' --confirm
+  ''', desc= "Drag File(s)"},
+  # Open the windows File explorer property Tab
+    { on = [ "c", "P" ], exec= '''
+    shell 'pwsh.exe -ex Bypass -C "& $env:APPDATA\yazi\WindowsEnhancement\OpenFileDetails.ps1 %1' --confirm
+  ''', desc= "Open File Details"},
+  # Open Powershell As User
+    { on= [ "g", "p" ], exec= '''
+    shell 'pwsh.exe -ex Bypass -C "& env:APPDATA\yazi\WindowsEnhancement\PowershellSession.ps1 %1 "' --confirm
+  ''', desc= "Open PowerShell Here"},
+  # Open Powershell As Admin / Will Prompt UAC 
+  # TODO : Open in default terminal not classic powershell
+  {  on= [ "g", "P" ], exec= '''
+    shell 'pwsh.exe -ex Bypass -C "& $env:APPDATA\yazi\WindowsEnhancement\PowershellSession.ps1 -Admin %1 "' --confirm
+  ''', desc= "Open PowerShell as Admin" },
+  # Open in $EDITOR inside TMUX on WSLSIDE 
+  # Create a new tmux session from filename , cd into parent folder, open it in choosen editor
+  # The script OpenInWslTmux.ps1 need to be edited with name of the app 
+  {  on= [ "g", "T" ], exec= '''
+    shell 'pwsh.exe -ex Bypass -C "& $env:APPDATA\yazi\WindowsEnhancement\OpenInWSLTmux.ps1 %1"' --confirm
+  ''', desc= "Open File In TMUX EDITOR" },
+```
+
+# Wezterm install
+
+To work with `Yazi` Wezterm is highly recommanded to get good image preview.
+
+## Basic install
+To install Wezterm with winget:
+
+```powershell
+winget install wez.wezterm
+```
+TODO: Nighty with Github to use latest features
